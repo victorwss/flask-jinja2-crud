e1b1f03d13e4deba4010b6207b8ccfc4d8a2d645
Victor Williams Stafusa da Silva <victorwssilva@gmail.com>
2020-08-17 12:33:40 -0300

    Comentários.

--- serie.py
+++ serie.py
@@ -4,145 +4,256 @@ import sqlite3
 import os
 import werkzeug
 
+# Observação: O código abaixo não contém uma estrutura dividida em camadas com blueprints, services, controllers, DAOs, models, etc., pois a ideia é tentar manter tudo bem simples.
+#             Quando você estiver trabalhando em seu projeto real, tente separar isso tudo.
+#             E idealmente num projeto grande, você teria um serviço fornecendo dados em formato JSON e alguma outra coisa utilizando-os para gerar o HTML, mas aqui está tudo numa coisa só.
+
 ############################
 #### Definições da API. ####
 ############################
 
+# Cria o objeto principal do Flask.
 app = Flask(__name__)
+
+# Quase todos os métodos terão estas três linhas para se certificar de que o login é válido. Se não for, o usuário será redirecionado para a tela de login.
+#   logado = autenticar_login()
+#   if logado is None:
+#       return redirect("/")
+#
+# Esse código é bastante repetitivo, o que é bem chato. Até é possível resolver isso movendo-o para method decorators e/ou filtros, no entanto, para não complicarmos demais, vamos deixá-lo assim.
+
+# Os métodos desta camada tem que ser idealmente um tanto "burros". Eles APENAS devem fazer o seguinte e nada mais:
+# 1. Autenticação.
+# 2. Retirar os dados relevantes da requisição.
+# 3. Chamar alguma função no banco de dados ou na regra de negócio que faz o trabalho pesado.
+# 4. Montar uma resposta à requisição.
+
+# É importante notar-se que não devem haver regras de negócio aqui.
+# Todo o trabalho que essas funções executam deve ser APENAS o de ler a requisição e escrever a resposta, delegando o trabalho de processamento às regras de negócio.
+
+### Partes de login. ###
 
 @app.route("/")
 @app.route("/login")
 def menu():
+    # Autenticação.
     logado = autenticar_login()
     if logado is None:
         return render_template("/login.html", erro = "")
+
+    # Monta a resposta.
     return render_template("menu.html", logado = logado, mensagem = "")
-
-@app.route("/serie")
-def listar_series_api():
-    logado = autenticar_login()
-    if logado is None:
-        return redirect("/")
-    return render_template("lista_series.html", logado = logado, series = db_listar_series())
-
-@app.route("/serie/novo", methods = ["GET"])
-def form_criar_serie_api():
-    logado = autenticar_login()
-    if logado is None:
-        return redirect("/")
-    return render_template("form_serie.html", logado = logado)
-
-@app.route("/serie/novo", methods = ["POST"])
-def criar_serie_api():
-    logado = autenticar_login()
-    if logado is None:
-        return redirect("/")
-    numero = request.form["numero"]
-    turma = request.form["turma"]
-    ja_existia, serie = criar_serie(numero, turma)
-    mensagem = f"A série {numero}{turma} já existia com o id {serie['id_serie']}." if ja_existia else f"A série {numero}{turma} criada com id {serie['id_serie']}."
-    return render_template("menu.html", logado = logado, mensagem = mensagem)
-
-@app.route("/aluno")
-def listar_alunos_api():
-    logado = autenticar_login()
-    if logado is None:
-        return redirect("/")
-    return render_template("lista_alunos.html", logado = logado, alunos = db_listar_alunos())
-
-@app.route("/aluno/novo", methods = ["GET"])
-def form_criar_aluno_api():
-    logado = autenticar_login()
-    if logado is None:
-        return redirect("/")
-    aluno = {'id_aluno': 'novo', 'nome': '', 'sexo': '', 'id_serie': '', 'id_foto': ''}
-    return render_template("form_aluno.html", logado = logado, aluno = aluno, series = db_listar_series_ordem())
-
-@app.route("/aluno/<int:id_aluno>", methods = ["GET"])
-def form_alterar_aluno_api(id_aluno):
-    logado = autenticar_login()
-    if logado is None:
-        return redirect("/")
-    aluno = db_consultar_aluno(id_aluno)
-    if aluno == None:
-        return render_template("menu.html", logado = logado, mensagem = f"Esse aluno não existe."), 404
-    return render_template("form_aluno.html", logado = logado, aluno = aluno, series = db_listar_series_ordem())
-
-@app.route("/aluno/novo", methods = ["GET", "POST"])
-def criar_aluno_api():
-    logado = autenticar_login()
-    if request.method != "POST": return "", 405
-    if logado is None:
-        return redirect("/")
-    nome = request.form["nome"]
-    sexo = request.form["sexo"]
-    id_serie = request.form["id_serie"]
-    aluno = criar_aluno(nome, sexo, id_serie, salvar_arquivo_upload)
-    return render_template("menu.html", logado = logado, mensagem = f"O aluno {nome} foi criado com o id {aluno['id_aluno']}." if sexo == "M" else f"A aluna {nome} foi criada com o id {aluno['id_aluno']}.")
-
-@app.route("/aluno/<int:id_aluno>", methods = ["GET", "POST"])
-def editar_aluno_api(id_aluno):
-    if request.method != "POST": return "", 405
-    logado = autenticar_login()
-    if logado is None:
-        return redirect("/")
-    nome = request.form["nome"]
-    sexo = request.form["sexo"]
-    id_serie = request.form["id_serie"]
-    status, aluno = editar_aluno(id_aluno, nome, sexo, id_serie, salvar_arquivo_upload, deletar_foto)
-    if status == 'não existe':
-        return render_template("menu.html", logado = logado, mensagem = "Esse aluno nem mesmo existia mais." if sexo == "M" else "Essa aluna nem mesmo existia mais."), 404
-    return render_template("menu.html", logado = logado, mensagem = f"O aluno {nome} com o id {id_aluno} foi editado." if sexo == "M" else f"A aluna {nome} com o id {id_aluno} foi editada.")
-
-@app.route("/aluno/<int:id_aluno>", methods = ["DELETE"])
-def deletar_aluno_api(id_aluno):
-    logado = autenticar_login()
-    if logado is None:
-        return redirect("/")
-    aluno = apagar_aluno(id_aluno)
-    if aluno == None:
-        return render_template("menu.html", logado = logado, mensagem = "Esse aluno nem mesmo existia mais."), 404
-    return render_template("menu.html", logado = logado, mensagem = f"O aluno com o id {id_aluno} foi excluído." if aluno['sexo'] == "M" else f"A aluna com o id {id_aluno} foi excluída.")
-
-@app.route("/aluno/foto/<id_foto>")
-def aluno_download_foto(id_foto):
-    logado = autenticar_login()
-    if logado is None:
-        return redirect("/")
-    try:
-        return send_from_directory("alunos_fotos", id_foto)
-    except werkzeug.exceptions.NotFound as x:
-        return send_from_directory("static", "no-photo.png")
-
-@app.route("/aluno/foto/<id_foto>", methods = ["DELETE"])
-def aluno_deletar_foto(id_foto):
-    logado = autenticar_login()
-    if logado is None:
-        return redirect("/")
-    deletar_foto(id_foto)
-    return ""
 
 @app.route("/login", methods = ["POST"])
 def login():
+    # Extrai os dados do formulário.
     f = request.form
     if "login" not in f or "senha" not in f:
         return ":(", 422
     login = f["login"]
     senha = f["senha"]
+
+    # Faz o processamento.
     logado = db_fazer_login(login, senha)
+
+    # Monta a resposta.
     if logado is None:
         return render_template("login.html", erro = "Ops. A senha estava errada.")
     resposta = make_response(redirect("/"))
+
+    # Armazena o login realizado com sucesso em cookies (autenticação).
     resposta.set_cookie("login", login, samesite = "Strict")
     resposta.set_cookie("senha", senha, samesite = "Strict")
     return resposta
 
 @app.route("/logout", methods = ["POST"])
 def logout():
+    # Monta a resposta.
     resposta = make_response(render_template("login.html", mensagem = "Tchau."))
+
+    # Limpa os cookies com os dados de login (autenticação).
     resposta.set_cookie("login", "", samesite = "Strict")
     resposta.set_cookie("senha", "", samesite = "Strict")
     return resposta
+
+### Cadastro de séries. ###
+
+# Tela de listagem de séries.
+@app.route("/serie")
+def listar_series_api():
+    # Autenticação.
+    logado = autenticar_login()
+    if logado is None:
+        return redirect("/")
+
+    # Faz o processamento.
+    lista = db_listar_series()
+
+    # Monta a resposta.
+    return render_template("lista_series.html", logado = logado, series = lista)
+
+# Tela com o formulário de criação de séries.
+@app.route("/serie/novo", methods = ["GET"])
+def form_criar_serie_api():
+    # Autenticação.
+    logado = autenticar_login()
+    if logado is None:
+        return redirect("/")
+
+    # Monta a resposta.
+    return render_template("form_serie.html", logado = logado)
+
+# Processa o formulário de criação de séries.
+@app.route("/serie/novo", methods = ["POST"])
+def criar_serie_api():
+    # Autenticação.
+    logado = autenticar_login()
+    if logado is None:
+        return redirect("/")
+
+    # Extrai os dados do formulário.
+    numero = request.form["numero"]
+    turma = request.form["turma"]
+
+    # Faz o processamento.
+    ja_existia, serie = criar_serie(numero, turma)
+
+    # Monta a resposta.
+    mensagem = f"A série {numero}{turma} já existia com o id {serie['id_serie']}." if ja_existia else f"A série {numero}{turma} criada com id {serie['id_serie']}."
+    return render_template("menu.html", logado = logado, mensagem = mensagem)
+
+### Cadastro de alunos. ###
+
+# Tela de listagem de alunos.
+@app.route("/aluno")
+def listar_alunos_api():
+    # Autenticação.
+    logado = autenticar_login()
+    if logado is None:
+        return redirect("/")
+
+    # Faz o processamento.
+    lista = db_listar_alunos()
+
+    # Monta a resposta.
+    return render_template("lista_alunos.html", logado = logado, alunos = lista)
+
+# Tela com o formulário de criação de um novo aluno.
+@app.route("/aluno/novo", methods = ["GET"])
+def form_criar_aluno_api():
+    # Autenticação.
+    logado = autenticar_login()
+    if logado is None:
+        return redirect("/")
+
+    # Faz o processamento.
+    lista = db_listar_series_ordem()
+    aluno = {'id_aluno': 'novo', 'nome': '', 'sexo': '', 'id_serie': '', 'id_foto': ''}
+
+    # Monta a resposta.
+    return render_template("form_aluno.html", logado = logado, aluno = aluno, series = lista)
+
+# Tela com o formulário de alteração de um aluno existente.
+@app.route("/aluno/<int:id_aluno>", methods = ["GET"])
+def form_alterar_aluno_api(id_aluno):
+    # Autenticação.
+    logado = autenticar_login()
+    if logado is None:
+        return redirect("/")
+
+    # Faz o processamento.
+    aluno = db_consultar_aluno(id_aluno)
+
+    # Monta a resposta.
+    if aluno == None:
+        return render_template("menu.html", logado = logado, mensagem = f"Esse aluno não existe."), 404
+    return render_template("form_aluno.html", logado = logado, aluno = aluno, series = db_listar_series_ordem())
+
+# Processa o formulário de criação de alunos. Inclui upload de fotos.
+@app.route("/aluno/novo", methods = ["POST"])
+def criar_aluno_api():
+    # Autenticação.
+    logado = autenticar_login()
+    if logado is None:
+        return redirect("/")
+
+    # Extrai os dados do formulário.
+    nome = request.form["nome"]
+    sexo = request.form["sexo"]
+    id_serie = request.form["id_serie"]
+
+    # Faz o processamento.
+    aluno = criar_aluno(nome, sexo, id_serie, salvar_arquivo_upload)
+
+    # Monta a resposta.
+    return render_template("menu.html", logado = logado, mensagem = f"O aluno {nome} foi criado com o id {aluno['id_aluno']}." if sexo == "M" else f"A aluna {nome} foi criada com o id {aluno['id_aluno']}.")
+
+# Processa o formulário de alteração de alunos. Inclui upload de fotos.
+@app.route("/aluno/<int:id_aluno>", methods = ["POST"])
+def editar_aluno_api(id_aluno):
+    # Autenticação.
+    logado = autenticar_login()
+    if logado is None:
+        return redirect("/")
+
+    # Extrai os dados do formulário.
+    nome = request.form["nome"]
+    sexo = request.form["sexo"]
+    id_serie = request.form["id_serie"]
+
+    # Faz o processamento.
+    status, aluno = editar_aluno(id_aluno, nome, sexo, id_serie, salvar_arquivo_upload, deletar_foto)
+
+    # Monta a resposta.
+    if status == 'não existe':
+        return render_template("menu.html", logado = logado, mensagem = "Esse aluno nem mesmo existia mais." if sexo == "M" else "Essa aluna nem mesmo existia mais."), 404
+    return render_template("menu.html", logado = logado, mensagem = f"O aluno {nome} com o id {id_aluno} foi editado." if sexo == "M" else f"A aluna {nome} com o id {id_aluno} foi editada.")
+
+# Processa o botão de excluir um aluno.
+@app.route("/aluno/<int:id_aluno>", methods = ["DELETE"])
+def deletar_aluno_api(id_aluno):
+    # Autenticação.
+    logado = autenticar_login()
+    if logado is None:
+        return redirect("/")
+
+    # Faz o processamento.
+    aluno = apagar_aluno(id_aluno)
+
+    # Monta a resposta.
+    if aluno == None:
+        return render_template("menu.html", logado = logado, mensagem = "Esse aluno nem mesmo existia mais."), 404
+    return render_template("menu.html", logado = logado, mensagem = f"O aluno com o id {id_aluno} foi excluído." if aluno['sexo'] == "M" else f"A aluna com o id {id_aluno} foi excluída.")
+
+### Fotos dos alunos. ###
+
+# Faz o download de uma foto.
+@app.route("/aluno/foto/<id_foto>")
+def aluno_download_foto(id_foto):
+    # Autenticação.
+    logado = autenticar_login()
+    if logado is None:
+        return redirect("/")
+
+    # Monta a resposta.
+    try:
+        return send_from_directory("alunos_fotos", id_foto)
+    except werkzeug.exceptions.NotFound as x:
+        return send_from_directory("static", "no-photo.png")
+
+# Deleta uma foto.
+@app.route("/aluno/foto/<id_foto>", methods = ["DELETE"])
+def aluno_deletar_foto(id_foto):
+    # Autenticação.
+    logado = autenticar_login()
+    if logado is None:
+        return redirect("/")
+
+    # Faz o processamento.
+    deletar_foto(id_foto)
+
+    # Monta a resposta.
+    return ""
 
 ###############################################
 #### Coisas internas da controller da API. ####
@@ -260,10 +371,10 @@ REPLACE INTO usuario (login, senha, nome) VALUES ('spiderman', 'aranha', 'Peter 
 REPLACE INTO usuario (login, senha, nome) VALUES ('batman', 'morcego', 'Bruce Wayne');
 """
 
-# Observação 1: A tabela usuario não utiliza a forma mais segura de se armazenar senhas. Isso será abordado mais para frente!
+# Observação: A tabela "usuario" acima não utiliza uma forma segura de se armazenar senhas. Isso será abordado mais para frente!
 
-# Observação 2: O código abaixo não contém uma estrutura dividida em camadas com blueprints, services, controllers, DAOs, models, etc., pois a ideia é tentar manter tudo bem simples.
-#               Quando você estiver trabalhando em seu projeto real, tente separar isso tudo.
+# Observação: Os métodos do DAO devem ser "burros". Eles apenas executam alguma instrução no banco de dados e nada mais.
+#             Não devem ter inteligência, pois qualquer tipo de inteligência provavelmente trata-se de uma regra de negócio, e que portanto não deve ficar no DAO.
 
 def conectar():
     return sqlite3.connect('serie.db')
